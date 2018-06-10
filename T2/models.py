6d705391c28db993b5a856d755c5ea1bf32abca7
gstvo <gustavo.borgesfr@gmail.com>
2018-06-10 00:06:27 -0300

    mais tweaks no método do first, aparentemente quase pronto, faltando apenas o caso do vai e volta

--- T2/models.py
+++ T2/models.py
@@ -12,20 +12,19 @@ class ContextFreeGrammar:
 	def decompose_and_formalize(self):
 		symbols = []
 		for p in self.productions:
-			if p not in [symbol.label for symbol in symbols] and p.isalpha():
+			if p not in [symbol.label for symbol in symbols] and (p.isalpha() or p == "&"):
 				symbols.append(Symbol(p))
 		for s in symbols:
-			if s.label.isalpha():
+			if s.label.isalpha() or s.label == "&":
 				if s.label.isupper():
 					self.vn.add(s)
 				else:
 					self.vt.add(s)
-
 		self.S = symbols[0]
 
 	def print_stuff(self):
-		print("".join(str(self.vn)))
-		print("".join(str(self.vt)))
+		print(str(self.vn))
+		print(str(self.vt))
 		print("Inicial: "+str(self.S))
 		self.S.set_firsts(self)
 		self.S.print_firsts()
@@ -53,19 +52,41 @@ class Symbol:
 			that_prod = that_prod.split("->")[1]
 			for x in range(len(that_prod)):
 				if that_prod[x] in [y.label for y in vt]: 
-					if x-1 == -1 or that_prod[(x-1)] == "|": #ou se o first do simbolo anterior conta um epsilon
+					if x-1 == -1 or that_prod[(x-1)] == "|":
 						symbol = next(s for s in vt if s.label == that_prod[x])
 						self.first.add(symbol)
-				elif that_prod[x] in [y.label for y in vn]:
-					if x-1 == -1 or that_prod[(x-1)] == "|" #ou se o first do simbolo anterior conta um epsilon:
+
+			for x in range(len(that_prod)):
+				if that_prod[x] in [y.label for y in vn]:
+					if x-1 == -1 or that_prod[(x-1)] == "|":
 						symbol = next(s for s in vn if s.label == that_prod[x])
 						print(symbol)
-						symbol.set_firsts(cfg)
-						self.first.update(symbol.first)
+						if symbol.label != self.label:
+							symbol.set_firsts(cfg)
+							self.first.update(symbol.first)
+						if "&" in [y.label for y in symbol.first]:
+							epsilon = next(s for s in symbol.first if s.label == "&")
+							self.first.remove(epsilon)
+							index = x
+							while True:
+								if (index+1) <= len(that_prod)-1 and that_prod[index+1] != "|" :
+									next_symbol = next(s for s in vt|vn if s.label == that_prod[index+1])
+								else:
+									self.first.add(epsilon)
+									break
+								if "&" in that_prod:
+									self.first.add(epsilon)
+								if next_symbol in vt:
+									self.first.add(next_symbol)
+									break
+								else:
+									if next_symbol.label != self.label:
+										next_symbol.set_firsts(cfg)
+										self.first.update(next_symbol.first)
+									index+=1
 	def print_firsts(self):
-		print("".join(str(self.first)))
-					
+		print(str(self.first))
 
-grammar = "S->aaAAA|bBBBB|cCCCCCC|Ad\nA->e"
+grammar = "S->Sa|bB|cC|Ad|\nA->BBAw|h|&\nB->f|&"
 cfg = ContextFreeGrammar(grammar)
 cfg.print_stuff()
