3f6d2f9f714e77ffba83ed78aba0788d3f62a3a2
gstvo <gustavo.borgesfr@gmail.com>
2018-06-13 17:13:18 -0300

    mais alterações, rever o negócio do epsilon

--- T2/models.py
+++ T2/models.py
@@ -39,39 +39,23 @@ class ContextFreeGrammar:
 
 		self.S = symbols[0]
 
-	def print_stuff(self):
-		print(str(self.vn))
-		print(str(self.vt))
-		print("Inicial: "+str(self.S))
-		self.S.set_firsts(self)
-		self.S.print_firsts()
-
-class Symbol:
-	def __init__(self, label):
-		self.label = label
-		self.first = set()
-		self.follow = set()
-
-	def __repr__(self):
-		return self.label
-
-	def set_firsts(self, cfg):
+	def set_firsts(self, X):
 		p = cfg.productions
 		vn = cfg.vn
 		vt = cfg.vt
-		if self in vt:
-			self.first.add(self)
+		if X in vt:
+			X.first.add(X)
 		else:
 			prods = p.split("\n")
 			print(prods)
-			that_prod = next(p for p in prods if (p.split("->")[0]) == self.label)
+			that_prod = next(p for p in prods if (p.split("->")[0]) == X.label)
 			that_prod = that_prod.split("->")[1]
 			print(that_prod)
 			for x in range(len(that_prod)):
 				if that_prod[x] in [y.label for y in vt]: 
 					if x-1 == -1 or that_prod[(x-1)] == "|":
 						symbol = next(s for s in vt if s.label == that_prod[x])
-						self.first.add(symbol)
+						X.first.add(symbol)
 			# se X->S1 S2 S3
 			# first(S1) ta em X
 			# se epsilon ta em S1, first(S2) ta em X e assim vai
@@ -84,22 +68,47 @@ class Symbol:
 				addedTerminal = False
 				for k in range(len(y)):
 					if y[k] in [z.label for z in vn]:
-						symbol = next(s for s in vn if s.label == y[k])
-						if y[k] != self.label:
-							symbol.set_firsts(cfg)
-							self.first.update(symbol.first)
-						if "&" in [l.label for l in symbol.first]:
-							epsilon = next(s for s in symbol.first if s.label == "&")
-							self.first.remove(epsilon)
+						yn = next(s for s in vn if s.label == y[k])
+						if y[k] != X.label:
+							self.set_firsts(yn)
+							X.first.update(yn.first)
+						if "&" in [l.label for l in yn.first]:
+							epsilon = next(s for s in yn.first if s.label == "&")
+							X.first.remove(epsilon)
 							previousHadEpsilon=True
 						else:
 							previousHadEpsilon=False
 					elif y[k] in [s.label for s in vt]:
 						if previousHadEpsilon: 
-							self.first.add(next(s for s in vt if s.label == y[k]))
+							X.first.add(next(s for s in vt if s.label == y[k]))
 							addedTerminal=True
 				if not addedTerminal:
-					self.first.add(next(s for s in vt if s.label == "&"))
+					X.first.add(next(s for s in vt if s.label == "&"))
+
+	def print_stuff(self):
+		print(str(self.vn))
+		print(str(self.vt))
+		print("Inicial: "+str(self.S))
+		self.set_firsts(self.S)
+		self.S.print_firsts()
+
+class Symbol:
+	def __init__(self, label):
+		self.label = label
+		self.first = set()
+		self.follow = set()
+
+	def __repr__(self):
+		return self.label
+
+	
+	# def set_follows(self, cfg):
+	# 	#passo1
+	# 	p = cfg.productions
+	# 	vn = cfg.vn
+	# 	vt = cfg.vt
+	# 	if self.label == cfg.S.label:
+
 	def print_firsts(self):
 		print(str(self.first))
 
